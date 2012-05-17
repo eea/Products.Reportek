11550fe5dde2f6a6ba27d7bb06ea6e62d89fa9d8
moregale <moregale@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-05-17 09:16:38 +0000

    remove old XML code

--- Products/Reportek/XMLInfoParser.py
+++ Products/Reportek/XMLInfoParser.py
@@ -26,88 +26,10 @@ $Id$"""
 
 __version__='$Revision$'[11:-2]
 
-from xml.sax.handler import ContentHandler, feature_namespaces
-from xml.sax.saxlib import LexicalHandler
+from xml.sax.handler import ContentHandler
 from xml.sax import handler, make_parser, InputSource
 from cStringIO import StringIO
 import lxml.etree
-
-class InfoStructureHandler(ContentHandler, LexicalHandler):
-    """ """
-
-    __XSI_NS = 'http://www.w3.org/2001/XMLSchema-instance'
-    __SCHEMALOCATION_TAG = 'schemaLocation'
-    __NONAMESPACESCHEMALOCATION_TAG = 'noNamespaceSchemaLocation'
-
-    def __init__(self):
-        """ """
-        self.__isroot = 1
-        self.xdi_info = 0
-        self.xdi_name = None
-        self.xdi_public_id = None
-        self.xdi_system_id = None
-        self.xsi_info = 0
-        self.xsi_xmlns = {}
-        self.xsi_schema_location = None
-
-    def startDTD(self, name, public_id, system_id):
-        """ get DTD information """
-        self.xdi_info = 1
-        self.xdi_name = name
-        if public_id:
-            self.xdi_public_id = public_id
-            self.xdi_system_id = system_id
-        elif system_id:
-            self.xdi_system_id = system_id
-
-    def startElementNS(self, name, qname, attributes):
-        """ """
-        if self.__isroot: # Only continue if we're in the root element
-            self.__isroot = 0
-            # Check no-namespace schema attribute
-            value = attributes.get((self.__XSI_NS, self.__NONAMESPACESCHEMALOCATION_TAG))
-            if value is not None:
-                raise NotImplementedError
-                self.xsi_info = 1
-                self.xsi_schema_location = value.strip()
-                return
-            # Check schema attribute
-            value = attributes.get((self.__XSI_NS, self.__SCHEMALOCATION_TAG))
-            if value is not None:
-                self.xsi_info = 1
-                u = value.strip().split()
-                if len(u) == 1:
-                    raise NotImplementedError
-                    self.xsi_schema_location = value.strip()
-                else:
-                    raise NotImplementedError
-                    s = []
-                    for x in xrange(1,len(u),2):
-                        s.append(u[x])
-                    self.xsi_schema_location = ' '.join(s)
-                return
-
-class XMLInfoParser:
-    """ """
-
-    def __init__(self):
-        """ """
-        pass
-
-    def ParseXmlFile(self, p_xml_string):
-        """ """
-        l_info_handler = InfoStructureHandler()
-        l_parser = make_parser()
-        l_parser.setContentHandler(l_info_handler)
-        l_parser.setProperty(handler.property_lexical_handler, l_info_handler)
-        l_inpsrc = InputSource()
-        l_inpsrc.setByteStream(StringIO(p_xml_string))
-        try:
-            l_parser.setFeature(feature_namespaces, 1)
-            l_parser.parse(l_inpsrc)
-            return l_info_handler
-        except:
-            return None
 
 
 class ElementHandler(ContentHandler):
