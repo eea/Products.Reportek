6d90cad30d8a014fe81d9f01524832e91ccdb31f
dincamih <dincamih@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-12-04 12:23:02 +0000

    Build_collections test with GET

--- Products/Reportek/tests/testReportekEngine.py
+++ Products/Reportek/tests/testReportekEngine.py
@@ -508,8 +508,8 @@ class ReportekEngineTest(_BaseTest):
         self.engine.manage_editEngine(title='After Title', REQUEST=self.root.REQUEST)
         self.assertEqual('After Title', self.engine.title)
 
-    def test_Build_collections(self):
-        self.root.REQUEST.method = Mock(return_value='POST')
+    def test_Build_collections_with_GET(self):
+        self.root.REQUEST.method = 'GET'
         self.engine.ZopeTime = Mock(return_value=DateTime())
         self.root.standard_html_header = ''
         self.root.standard_html_footer = ''
@@ -537,7 +537,38 @@ class ReportekEngineTest(_BaseTest):
             cid='',
             REQUEST=self.root.REQUEST
         )
-        self.assertEqual(len(self.root.at.objectIds()), 1)
+        self.assertEqual(0, len(self.root.at.objectIds()))
+
+    def test_Build_collections_with_POST(self):
+        self.root.REQUEST.method = 'POST'
+        self.engine.ZopeTime = Mock(return_value=DateTime())
+        self.root.standard_html_header = ''
+        self.root.standard_html_footer = ''
+        localities = {
+            'http://rod.eionet.eu.int/spatial/3': {
+                'iso': 'AT',
+                'name': 'Austria',
+                'uri': 'http://rod.eionet.eu.int/spatial/3'
+            }
+        }
+        self.engine.localities_dict = Mock(return_value=localities)
+        self.root.localities_table = Mock(return_value=[])
+        self.root.dataflow_table = Mock(return_value=[])
+        self.root._setObject( 'at', Collection('at',
+            'Austria', '', '', '',
+            'http://rod.eionet.eu.int/spatial/3',
+            '', '',
+            ['http://example.com/dataflow/1'],
+            allow_collections=0, allow_envelopes=1))
+
+        self.engine.Build_collections(
+            ccountries = ['http://rod.eionet.eu.int/spatial/3'],
+            ctitle='Test collection',
+            cobligation= ['http://example.com/dataflow/1'],
+            cid='',
+            REQUEST=self.root.REQUEST
+        )
+        self.assertEqual(1, len(self.root.at.objectIds()))
 
 class SearchResultsTest(_BaseTest):
 
