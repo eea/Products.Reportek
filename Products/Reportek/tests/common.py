290e7c60321f50f6e89e0e7828826c7db3d602fd
dincamih <dincamih@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-02-19 14:15:47 +0000

    add new useful testcase class

--- Products/Reportek/tests/common.py
+++ Products/Reportek/tests/common.py
@@ -1,6 +1,15 @@
 import unittest
+from mock import Mock
+
 from StringIO import StringIO
 from utils import simple_addEnvelope
+
+from OFS.Folder import Folder
+from OFS.SimpleItem import SimpleItem
+from Products.Reportek.Collection import Collection
+from Products.Reportek.Envelope import Envelope
+from Products.Reportek.process import process
+from Products.Reportek import exceptions
 
 
 def create_process(obj, p_id, dataflows=None, countries=None):
@@ -107,3 +116,52 @@ class _BaseTest(unittest.TestCase):
                             application='StartActivity')
         self.wf.process.begin = 'AutoBegin'
         self.wf.setProcessMappings('process', '1', '1')
+
+class _WorkflowTestCase(_BaseTest):
+
+    def create_cepaa_set(self, idx):
+        col_id = "col%s" %idx
+        env_id = "env%s" %idx
+        proc_id = "proc%s" %idx
+        act_id = "act%s" %idx
+        app_id = "act%s" %idx
+        country = 'http://spatial/%s' %idx
+        dataflow_uris = 'http://obligation/%idx' %idx
+        "create collection, envelope, process, activity, application"
+        col = Collection(col_id, country=country, dataflow_uris=dataflow_uris)
+        self.app._setOb(col_id, col)
+
+        self.app.Templates.StartActivity = Mock(return_value='Test Application')
+        self.app.Templates.StartActivity.title_or_id = Mock(return_value='Start Activity Template')
+        create_process(self, proc_id)
+        self.wf.addApplication(app_id, 'SomeFolder/%s' %app_id)
+
+        self.app.Applications._setOb(proc_id, Folder(proc_id))
+        proc = getattr(self.app.Applications, proc_id)
+
+        app = SimpleItem(app_id)
+        app.id = app_id
+        app.__call__ = Mock(return_value='Test Application')
+        proc._setOb(app_id, app)
+        getattr(proc, app_id).id = app_id
+        getattr(self.wf, proc_id).addActivity(act_id,
+                            split_mode='xor',
+                            join_mode='xor',
+                            start_mode=1)
+        getattr(self.wf, proc_id).begin = act_id
+        self.wf.setProcessMappings(proc_id, '1', '1')
+
+        env = Envelope(process=getattr(self.wf, proc_id),
+                       title='FirstEnvelope',
+                       authUser='TestUser',
+                       year=2012,
+                       endyear=2013,
+                       partofyear='January',
+                       country='http://spatial/1',
+                       locality='TestLocality',
+                       descr='TestDescription')
+        env.id = env_id
+        getattr(self.app, col_id)._setOb(env_id, env)
+        setattr(self, col_id, getattr(self.app, col_id))
+        setattr(self, env_id, getattr(getattr(self.app, col_id), env_id))
+        getattr(self, env_id).startInstance(self.app.REQUEST)
