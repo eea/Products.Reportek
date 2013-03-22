3f95b7019507d300b353352a06309b6c6fc0e4f6
dincamih <dincamih@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-03-22 13:25:57 +0000

    logupload.py external method

---
+++ extras/external_methods/logupload.py
@@ -0,0 +1,29 @@
+import os
+import time
+
+cwd = os.environ['CDR_DEMUPLOADS_LOG']
+
+def logtheupload(REQUEST):
+    """ Log a line """
+    f = open(cwd,'a')
+    f.write("\nNew upload at %s\n" % time.ctime())
+    f.write("User: %s\n" % REQUEST.AUTHENTICATED_USER.getUserName())
+    f.write("Content length: %s\n" % REQUEST['CONTENT_LENGTH'])
+    f.write("Client IP: %s\n" % REQUEST['HTTP_X_FORWARDED_FOR'])
+    if REQUEST.has_key('title'):
+        f.write("Title: %s\n" % REQUEST['title'])
+    if REQUEST.has_key('country'):
+        f.write("Country: %s\n" % REQUEST['country'])
+    f.close()
+
+def logtheend(finalurl='not disclosed'):
+    """ Log a line """
+    f = open(cwd,'a')
+    f.write("Uploaded to: %s\n" % finalurl)
+    f.write("Finished at %s\n" % time.ctime())
+    f.close()
+
+def showlog():
+    """ Show the log """
+    ret = open(cwd).read()
+    return ret.split('\n\n')
