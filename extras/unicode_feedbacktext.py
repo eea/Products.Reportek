e1f09ecb635fae027944a86fe2c254bddb8e1d2c
dincamih <dincamih@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-03-29 11:44:29 +0000

    convert feedback text to unicode

---
+++ extras/unicode_feedbacktext.py
@@ -0,0 +1,14 @@
+import transaction
+
+def decode_feedbacktext(root):
+    for item in root.Catalog({'meta_type': 'Report Feedback'}):
+        feedback = item.getObject()
+        text = feedback.feedbacktext
+        if not type(text) == type(unicode()):
+            try:
+                feedback.feedbacktext = text.decode('utf8')
+            except UnicodeDecodeError as err:
+                feedback.feedbacktext = text.decode('latin-1')
+    ans = raw_input('Commit?\n')
+    if ans == 'yes':
+        transaction.commit()
