5ea1da47e18db8fca2b2b125691b949888011fbc
baragdan <baragdan@24821446-e7ea-0310-81ac-b189f4507b0b>
2014-01-22 14:18:13 +0000

    Bring bdr po over to cdr, because this will be the main source base.

---
+++ Products/Reportek/locales/generate-xliff.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# You need translate-toolkit for this script to work
+# pip install translate-toolkit, or place it in buildout
+
+if [ -z "$1" ]; then
+	echo "Provide output directory"
+	exit 1
+fi
+
+out_dir=`readlink -f "$1"`
+if [ ! -d $out_dir ]; then
+	mkdir -p $out_dir
+else
+	echo "$out_dir already exits. exiting..."
+	exit 1
+fi
+cd $(dirname $0)
+
+po2xliff . $out_dir
+#find $out_dir -type f -name '*.xlf' -exec sed -i -e"s:<file\>\(.*\)>:<file\1 target-language=\"$(echo {}|sed -e's#^.*\(..\)/LC_MESSAGES/.*$#\1#g')\">:" {} \;
+for f in $out_dir/*; do
+	lang=`basename $f`
+	for xlf in `find $f -type f -name '*.xlf'`; do
+		echo "change file ${xlf}; put lang $lang in target-language"
+		sed -i -e"s:<file\>\(.*\)>:<file\1 target-language=\"${lang}\">:" ${xlf}
+	done
+done
+
+out_dir_small_name=`basename $out_dir`
+cd ${out_dir}/..
+tar -czf ${out_dir_small_name}.tar.gz $out_dir_small_name
+cd -
---
+++ Products/Reportek/locales/update_msg.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+""" Update empty msgstr with msgid
+"""
+
+import os
+
+def update(pofile):
+    """ Update po file
+    """
+    print "Updating po file: %s" % pofile
+
+    newfile = []
+    msgid = ""
+    msgstr = ""
+    count = 0
+    with open(pofile, 'r') as pobj:
+        lines = pobj.readlines()
+        for line in lines:
+            if not (line.startswith('msgid') or line.startswith("msgstr")):
+                newfile.append(line)
+
+            if line.startswith("msgid"):
+                msgid = line
+                newfile.append(line)
+                continue
+
+            if line.startswith("msgstr"):
+                if line.strip() != 'msgstr ""':
+                    newfile.append(line)
+                    continue
+                else:
+                    msgstr = msgid.replace("msgid", "msgstr", 1)
+                    newfile.append(msgstr)
+                    count += 1
+                    continue
+
+    os.rename(pofile, pofile + '.old')
+    with open(pofile, 'w') as pobj:
+        pobj.writelines(newfile)
+
+    print "\t changed %s broken msgstr" % count
+
+def main():
+    """ Main
+    """
+    langs = os.listdir('.')
+    for lang in langs:
+        if not os.path.isdir(lang):
+            continue
+
+        lc = os.path.join(lang, 'LC_MESSAGES')
+        if not os.path.isdir(lc):
+            continue
+
+        pos = [po for po in os.listdir(lc) if po.endswith('.po')]
+        for po in pos:
+            path = os.path.join(lc, po)
+            update(path)
+
+if __name__ == "__main__":
+    main()
