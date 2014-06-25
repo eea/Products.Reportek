c92cabd3a2e08baf93ab5e42a9ccc236cbbc4020
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2014-06-25 19:45:37 +0300

    Refs #19360 distribution of blobls by content-type analysis tool

--- Products/Reportek/updates/blob_analyze.py
+++ Products/Reportek/updates/blob_analyze.py
@@ -2,26 +2,79 @@
 #>>> from Products.Reportek.updates import blob_analyze
 #>>> blob_analyze.report(app, 'report.name')
 from ZODB.POSException import POSKeyError
-from Products.Reportek.blob import FileContainer
+#from Products.Reportek.blob import FileContainer
 
-from collection import defaultdict
+from collections import defaultdict
+import pprint
 
 __all__ = ['update']
 
 
-class DocStats(object):
+class RepDocStats(object):
     def __init__(self):
-        self.nr = 0
+        self.count = 0
         self.size = 0
 
     def add(self, size):
-        self.nr += 1
+        self.count += 1
         self.size += size
 
+    UNITS = ['B', 'KB', 'MB', 'GB', 'TB']
+    @classmethod
+    def human_readable(cls, size):
+        compact_size = size
+        step = 0
+        while compact_size > 1024 and step < len(cls.UNITS)-1:
+            compact_size /= 1024.0
+            step += 1
+        return "%.2f %s" % (compact_size, cls.UNITS[step])
+
+    def __str__(self):
+        return "{count: %d, size: %d (%s)}" % (self.count, self.size,
+                                            self.human_readable(self.size))
+
+    def __repr__(self):
+        return self.__str__()
+
 def report(app, out):
-    byDocumentType = defaultdict(DocStats)
-    blob_dir = FileContainer.get_blob_dir()
+    try:
+        o = open(out, 'w')
+    except:
+        import sys
+        o = sys.stdout
+        print "Can't open %s for write. Using stdout instead." % out
+
+    byDocumentType = defaultdict(RepDocStats)
+    byBlobType = defaultdict(RepDocStats)
+    byDocumentTypeMissingBlob = defaultdict(RepDocStats)
+    #blob_dir = FileContainer.get_blob_dir()
     for brain in app.Catalog(meta_type='Report Document'):
-        ob = brain.getObject()
-        data_file = getattr(ob, 'data_file', None)
+        missing = False
+
+        try:
+            doc = brain.getObject()
+
+            data_file = getattr(doc, 'data_file')
+            try:
+                file_handle = data_file._blob.open('r')
+                file_handle.close()
+            except POSKeyError:
+                missing = True
+
+
+            byDocumentType[doc.content_type].add(data_file.size)
+            byBlobType[data_file.content_type].add(data_file.size)
+            if missing:
+                byDocumentTypeMissingBlob[doc.content_type].add(data_file.size)
+        except Exception as e:
+            print str(e.args)
+
+    prn = pprint.PrettyPrinter(stream=o)
+    prn.pprint('byDocumentType:')
+    prn.pprint(dict(byDocumentType))
+    prn.pprint('byDocumentTypeMissingBlob:')
+    prn.pprint(dict(byDocumentTypeMissingBlob))
+    prn.pprint('byBlobType:')
+    prn.pprint(dict(byBlobType))
+    o.close()
 
