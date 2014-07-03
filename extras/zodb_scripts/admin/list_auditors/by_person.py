e14e0847e7592fe19ffacf2007b9fa5654e4c54f
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-07-03 16:33:25 +0300

    change zodb scripts: list_reporters and list_auditors

---
+++ extras/zodb_scripts/admin/list_auditors/by_person.py
@@ -0,0 +1,72 @@
+REQUEST = container.REQUEST
+RESPONSE = REQUEST.RESPONSE
+role = 'Auditor'
+
+
+def pathcompare(p1, p2):
+    return cmp(p1[0], p2[0])
+
+
+print context.standard_html_header(context, context.REQUEST)
+
+persons = {}
+results = []
+hits = container.Catalog(meta_type='Report Collection')
+for hit in hits:
+    obj = hit.getObject()
+    results.append((obj.absolute_url(0),
+                    '/' + obj.absolute_url(1),
+                    obj.bobobase_modification_time().Date(),
+                    obj.users_with_local_role(role),
+                    list(obj.dataflow_uris)
+    ))
+
+root_obj = context.restrictedTraverse(['', ])
+results.append((root_obj.absolute_url(0),
+                '/',
+                root_obj.bobobase_modification_time().Date(),
+                root_obj.users_with_local_role(role),
+                []
+))
+
+for hit in results:
+    members = hit[3]
+    if members != []:
+        for m in members:
+            if not persons.has_key(m):
+                persons[m] = []
+            persons[m].append(hit[1])
+
+print """
+<div id="tabbedmenu">
+  <ul>
+    <li><a href="/admin/list_auditors">Grouped by path</a></li>
+    <li id="currenttab"><span>Grouped by person</span></li>
+  </ul>
+</div>
+<div id="tabbedmenuend"></div> 
+<table class="datatable">
+<tr>
+  <th>%s</th>
+  <th>Path</th>
+</tr>""" % role
+
+evenstr = ''
+pitems = persons.items()
+pitems.sort()
+for account, paths in pitems:
+    err = ''
+    if string.find(account, ' ') >= 0:
+        err = 'Spaces&nbsp;in&nbsp;userid! '
+    print '''<tr%s><td valign="top">%s<a href="http://www.eionet.europa.eu/directory/user?uid=%s">%s</a></td>
+<td valign="top">%s</td>
+</tr>''' % (evenstr, err, account, account, string.join(paths, '<br/>') )
+    if evenstr == '':
+        evenstr = ' class="zebraeven"'
+    else:
+        evenstr = ''
+
+print "</table>"
+print context.standard_html_footer(context, context.REQUEST)
+
+return printed
