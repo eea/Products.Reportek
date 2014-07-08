9a1931caa70c486067dfa6ae95a21b199df51d3f
Ovidiu Miron <omiron1@yahoo.com>
2014-07-08 14:02:03 +0300

    added view for ReportekUtilities

--- Products/Reportek/ReportekUtilities.py
+++ Products/Reportek/ReportekUtilities.py
@@ -3,6 +3,8 @@ from AccessControl import ClassSecurityInfo
 import Products
 
 from constants import REPORTEK_UTILITIES
+from Products.PageTemplates.PageTemplateFile import PageTemplateFile
+from AccessControl.Permissions import view_management_screens
 
 
 class ReportekUtilities(Folder):
@@ -13,7 +15,10 @@ class ReportekUtilities(Folder):
     def __init__(self):
         self.id = REPORTEK_UTILITIES
 
-    def all_meta_types( self, interfaces=None ):
+    security.declareProtected(view_management_screens, 'index_html')
+    index_html = PageTemplateFile('zpt/admin', globals())
+
+    def all_meta_types(self, interfaces=None):
         """
             What can you put inside me? Checks if the legal products are
             actually installed in Zope
@@ -21,10 +26,3 @@ class ReportekUtilities(Folder):
         types = ['Script (Python)', 'Folder', 'Page Template']
 
         return [type for type in Products.meta_types if type['name'] in types]
-
-    def manage_properties(self):
-        """ Manage the edited values """
-        if self.REQUEST['REQUEST_METHOD'] == 'GET':
-            return self._manage_properties()
-
-        return self._manage_properties(manage_tabs_message="Properties changed")
