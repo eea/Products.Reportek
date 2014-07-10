b6fd8ad992d22bca91e8693afb42460dd4ee7ffc
Ovidiu Miron <omiron1@yahoo.com>
2014-07-10 14:11:42 +0300

    added utils.js #Refs 20358

---
+++ Products/Reportek/www/static/utils.js
@@ -0,0 +1,5 @@
+function activate_tab(){
+    document.getElementById(arguments[0])).style.display= "block";
+    for (var idx = 0; idx < arguments.length; idx++) {
+        document.getElementById(arguments[idx]).style.display= "none";
+}
