34b843d7294f94db9745355ca4be3073861fa9a5
Ovidiu Miron <omiron1@yahoo.com>
2014-07-16 18:40:46 +0300

    added populate datatable

--- Products/Reportek/views.py
+++ Products/Reportek/views.py
@@ -1,21 +1,25 @@
 from Products.Five import BrowserView
-import string
 import json
 
 
 class DataSources(BrowserView):
-    def __init__(self, context, request):
-        """
-        """
-        super(DataSources, self).__init__(context, request)
+    def process_data(self):
+        def format_row(path, url_path, last_change, obl, clients):
+            """
+            Format one datatable row.
+            """
+            return {"path": path,
+                    "url_path": url_path,
+                    "last_change": last_change,
+                    "obl": obl,
+                    "clients": clients}
 
-    def process(self):
         if self.context.REQUEST['REQUEST_METHOD'] == 'GET':
             """ form parameters
             """
-            obligation = self.context.REQUEST.get('obligation', None)
-            role = self.context.REQUEST.get('role', None)
-            #default for country is Romania only for test
+#            obligation = self.context.REQUEST.get('obligation', None)
+#            role = self.context.REQUEST.get('role', None)
+#            default for country is Romania only for test
             country = self.context.REQUEST.get('country', 'Romania')
 
             """datatables parameters
@@ -29,79 +33,18 @@ class DataSources(BrowserView):
                 getCountryName=country,
                 b_size=length,
                 b_start=start)
-
             results = []
             for hit in hits:
                 obj = hit.getObject()
-                results.append((obj.absolute_url(0), '/' +
-                                obj.absolute_url(1),
-                                obj.bobobase_modification_time().Date(),
-                                obj.users_with_local_role(role),
-                                list(obj.dataflow_uris)))
+                results.append(
+                    format_row(
+                        obj.absolute_url(0),
+                        '/' + obj.absolute_url(1),
+                        obj.bobobase_modification_time().Date(),
+                        "xxx", "yyy"))
 
-            data_to_return = {"recordsTotal": 90,
-                "draw":draw,
-                "data": results
-                }
+            data_to_return = {"recordsTotal": 90, "draw": draw, "data": results}
             return json.dumps(data_to_return)
-
-
-class ListClients(BrowserView):
-    """
-    """
-    def __init__(self, context, request):
-        super(ListClients, self).__init__(context, request)
-        self.persons = {}
-
-    def get_results(self, role):
-        def pathcompare(p1, p2):
-            return cmp(p1[0], p2[0])
-
-        hits = self.context.Catalog(meta_type='Report Collection')
-        results = []
-        for hit in hits:
-            obj = hit.getObject()
-            results.append((obj.absolute_url(0), '/' +
-                            obj.absolute_url(1),
-                            obj.bobobase_modification_time().Date(),
-                            obj.users_with_local_role(role),
-                            list(obj.dataflow_uris)))
-        root_obj = self.context.restrictedTraverse(['', ])
-        results.append((root_obj.absolute_url(0), '/',
-                        root_obj.bobobase_modification_time().Date(),
-                        root_obj.users_with_local_role(role), []))
-
-        results.sort(pathcompare)
-        return results
-
-    def get_obl_hover(self, hit):
-        obl = ""
-        hover = "0"
-        if len(hit[4]) > 0:
-            ol = []
-            for o in hit[4]:
-                ol.append(self.context.dataflow_lookup(o)['TITLE'])
-            obl = string.join(ol, '\n')
-            hover = str(len(hit[4]))
-        return (obl, hover)
-
-    def get_members(self, hit):
-        members = hit[3]
-        for member in members:
-            self.persons.setdefault(member, []).append(hit[1])
-            yield(self.context.get_person_uri(member), member)
-
-    def get_accounts_paths(self):
-        """
-        """
-        pers_items = self.persons.items()
-        pers_items.sort()
-        for account, paths in pers_items:
-            yield {'account': account,
-                   'paths': paths}
-
-    def get_person_uri(self, person):
-        return 'http://www.eionet.europa.eu/directory/user?uid=%s' % person
 
 
 class ListUsers(BrowserView):
--- Products/Reportek/www/static/utils.js
+++ Products/Reportek/www/static/utils.js
@@ -1,40 +1,20 @@
-function activate_tab() {
-    /*
-     * Implements the tab switching mechanism for 'tabbedmenu'
-     * Receives variable number of arguments.
-     * First argument is the id of the div how should be visible.
-     * The following are the div's id how should be hidden.
+function initDataTables(table_id) {
+    /*Init the datatable object used in zpt/utilities/list_users.zpt
      */
-    if (arguments.length < 1)
-        return;
-    document.getElementById(arguments[0]).style.display = "block";
-    for (var idx = 1; idx < arguments.length; idx++) {
-        document.getElementById(arguments[idx]).style.display= "none";
-    }
-    /*
-     * Than switch the tabs
-     */ 
-    switch_tabs();
-}
-
-function switch_tabs() {
-    /*
-     * TODO: make it more general
-     */
-    crtab = document.getElementById('currenttab');
-    next = document.getElementById('next');
-    crtab.removeAttribute("id");
-    next.setAttribute("id", 'currenttab');
-
-    /* remove ancor */
-    next.childNodes[1].childNodes[1].remove();
-    span_text = document.createTextNode("Grouped by person");
-    next.childNodes[1].appendChild(span_text);
-
-    /* add ancor */
-    ancor_text = document.createTextNode("Grouped by path");
-    ancor = document.createElement("a");
-    ancor.appendChild(ancor_text);
-    crtab.childNodes[1].remove();
-    crtab.appendChild(ancor);
+    $(table_id).DataTable({
+        "serverSide":true,
+        "ajax":'/data-source',
+        "columns":[
+            { "data": "path" },
+            { "data": "last_change" },
+            { "data": "obl" },
+            { "data": "clients" }
+        ],
+        "columnDefs":[{
+            "targets": 0,
+            "render": function(data, type, full, meta) {
+            return '<a href="'+full.url+'">'+data+'</a>';
+            }
+        } ]
+    });
 }
