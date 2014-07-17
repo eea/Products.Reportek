a216f0ff86f3679f6bae3423a5d2948c7c8c9629
Ștefan Daniel Mihăilă <stefan.mihaila@gmail.com>
2014-07-17 13:46:02 +0300

    Put back utils.js

---
+++ Products/Reportek/www/static/utils.js
@@ -0,0 +1,20 @@
+function initDataTables(table_id) {
+    /*Init the datatable object used in zpt/utilities/list_users.zpt
+     */
+    $(table_id).DataTable({
+        "serverSide":true,
+        "ajax":'/data-source',
+        "columns":[
+            { "data": "path" },
+            { "data": "last_change" },
+            { "data": "obl" },
+            { "data": "clients" }
+        ],
+        "columnDefs":[{
+            "targets": 0,
+            "render": function(data, type, full, meta) {
+            return '<a href="'+full.url_path+'">'+data+'</a>';
+            }
+        } ]
+    });
+}
