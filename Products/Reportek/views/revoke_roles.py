2f671ac34f43b1628a3b85d387366a9de38946cf
Ovidiu Miron <omiron1@yahoo.com>
2014-07-24 20:38:06 +0300

    Fixed crash when 'containing' contains too few characters Turned 'POST' method into 'GET' method
    Refs #20358

--- Products/Reportek/views/revoke_roles.py
+++ Products/Reportek/views/revoke_roles.py
@@ -2,6 +2,8 @@ from Products.Five import BrowserView
 
 
 class RevokeRoles(BrowserView):
+    """TODO
+    """
     def get_user_localroles(self, username):
         results = []
         for brain in self.context.Catalog(meta_type='Report Collection'):
@@ -20,3 +22,22 @@ class RevokeRoles(BrowserView):
         for path in paths:
             folder = self.context.restrictedTraverse(path)
             folder.manage_delLocalRoles(userids=[username, ])
+
+    def get_username_information(self):
+        """ Returns ('success'/'error', users_infs)
+        where:
+            users_infs is the result of the findUser()
+        """
+
+        containing = self.context.REQUEST.get('search_term')
+        matching_criteria = self.context.REQUEST.get('search_param')
+
+        users_infs = self.context.acl_users['ldapmultiplugin']['acl_users'].findUser(
+            search_param=matching_criteria, search_term=containing)
+        if users_infs[0]['sn'] == "Error":
+            return ('error', )
+
+        return ('success', users_infs)
+
+    def get_users_LDAPSchema(self):
+        return self.context.acl_users['ldapmultiplugin']['acl_users'].getLDAPSchema()
