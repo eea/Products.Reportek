789b011fbacc1309d972fe9680be251eb2e71b6c
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2014-07-31 12:54:24 +0300

    getUsers (list of collections grouped by user) Refs #20358

--- Products/Reportek/views/list_users.py
+++ Products/Reportek/views/list_users.py
@@ -1,4 +1,5 @@
 import json
+from copy import copy
 from base_admin import BaseAdmin
 
 
@@ -18,6 +19,7 @@ class ListUsers(BaseAdmin):
         return {o['uri']: o['TITLE'] for o
                 in self.context.dataflow_rod()}
 
+
     def search_catalog(self, obligations, countries, role):
         country_codes = self.get_country_codes(countries)
         dataflow_uris = [self.get_obligations[obl_id] for obl_id
@@ -34,17 +36,13 @@ class ListUsers(BaseAdmin):
 
         return self.context.Catalog(query)
 
-    def getUsersByPath(self, REQUEST):
 
+    def get_records(self, REQUEST):
         obligations = REQUEST.get('obligations', [])
         countries = REQUEST.get('countries', [])
         role = REQUEST.get('role', '')
 
-        brains = self.search_catalog(obligations, countries, role)
-
-        results = []
-
-        for brain in brains:
+        for brain in self.search_catalog(obligations, countries, role):
 
             obligations = []
             for uri in list(brain.dataflow_uris):
@@ -65,13 +63,31 @@ class ListUsers(BaseAdmin):
             if not users:
                 continue
 
-            # TODO: get user_urls
-            user_urls = ['#'] * len(users)
-
-            results.append({
+            yield {
                 'path': [brain.getPath(), brain.title],
                 'last_change': brain.bobobase_modification_time.Date(),
                 'obligations': obligations,
-                'users':  zip(user_urls, users)})
+                'users':  users}
 
-        return json.dumps({"data": results})
+
+    def getUsers(self, REQUEST):
+
+        records = []
+        for record in self.get_records(REQUEST):
+            res = copy(record)
+            del res['users']
+            for user in record['users']:
+                rr = copy(res)
+                rr['user'] = user
+                records.append(rr)
+
+        return json.dumps({"data": records})
+
+
+    def getUsersByPath(self, REQUEST):
+
+        records = []
+        for record in self.get_records(REQUEST):
+            records.append(record)
+
+        return json.dumps({"data": records})
