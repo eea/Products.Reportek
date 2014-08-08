e92a79e2bacd01306ef10dafe3ca1508b6c38eaf
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2014-08-08 16:52:29 +0300

    Refs #20358 Collections and Envelopes with wrong country ported to ReportekUtils

--- Products/Reportek/views/__init__.py
+++ Products/Reportek/views/__init__.py
@@ -3,3 +3,4 @@ from list_users import ListUsers
 from base_admin import BaseAdmin
 from manage_roles import ManageRoles
 from search_collections import SearchCollections
+from wrong_country import WrongCountry
