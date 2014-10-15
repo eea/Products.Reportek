2bd500f77a519a6d71b915bb7a5bd2b79c6adf3b
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-10-15 13:07:14 +0300

    Update script

---
+++ Products/Reportek/updates/migrate_scripts_from_db.py
@@ -0,0 +1,56 @@
+from Products.Reportek.XMLRPCMethod import XMLRPCMethod
+import transaction
+
+__all__ = ['update']
+
+
+def create_dataflow_rpc_call():
+    return XMLRPCMethod(
+        title='Get activities from ROD',
+        url='http://rod.eionet.europa.eu/rpcrouter',
+        method_name='WebRODService.getActivities',
+        timeout=10.0
+    )
+
+def create_localities_rpc_call():
+    return XMLRPCMethod(
+        title='Get countries from ROD',
+        url='http://rod.eionet.europa.eu/rpcrouter',
+        method_name='WebRODService.getCountries',
+        timeout=5.0
+    )
+
+
+def update(app):
+    if getattr(app, 'ReportekEngine', None):
+        if not getattr(app.ReportekEngine, 'xmlrpc_dataflow', None):
+            app.ReportekEngine.xmlrpc_dataflow = create_dataflow_rpc_call()
+            transaction.commit()
+        if not getattr(app.ReportekEngine, 'xmlrpc_localities', None):
+            app.ReportekEngine.xmlrpc_localities = create_localities_rpc_call()
+            transaction.commit()
+            
+    for brain in app.Catalog(meta_type=['Report Collection']):
+        obj = brain.getObject()
+
+        if not getattr(obj, 'xmlrpc_dataflow', None):
+            obj.xmlrpc_dataflow = create_dataflow_rpc_call()
+            transaction.commit()
+        if not getattr(obj, 'xmlrpc_localities', None):
+            obj.xmlrpc_localities = create_localities_rpc_call()
+            transaction.commit()
+
+    if getattr(app, 'dataflow_rod', None):
+        app._delObject("dataflow_rod", suppress_events=True)
+        transaction.commit()
+
+    if getattr(app, 'dataflow_dict', None):
+        app._delObject("dataflow_dict", suppress_events=True)
+        transaction.commit()
+
+    if getattr(app, 'dataflow_table', None):
+        app._delObject("dataflow_table", suppress_events=True)
+        transaction.commit()
+
+    if getattr(app, 'dataflow_lookup', None):
+        app._delObject("dataflow_lookup", suppress_events=True)
