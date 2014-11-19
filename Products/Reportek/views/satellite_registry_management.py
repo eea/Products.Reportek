33615f1292a4db2c66f54cb69250afa306d86d1f
Vitalie Maldur <maldur.vitalik@gmail.com>
2014-11-19 16:45:12 +0200

    Company details in format xml, refs #21390

--- Products/Reportek/views/satellite_registry_management.py
+++ Products/Reportek/views/satellite_registry_management.py
@@ -3,10 +3,8 @@ import requests
 
 class SatelliteRegistryManagement(BaseAdmin):
     """ RegistryManagement view """
-
     def get_companies(self):
-        #response = requests.get("https://bdr-test.eionet.europa.eu/_cache/undertaking/list", verify=False)
-        response = requests.get("http://eftimie.ro/fapi/undertaking/list")
+        response = requests.get("https://bdr-test.eionet.europa.eu/_cache/undertaking/list", verify=False)
         if response.status_code == requests.codes.ok:
             return response.json()
         return []
@@ -14,8 +12,7 @@ class SatelliteRegistryManagement(BaseAdmin):
     def get_company_details(self):
         if self.request.get('id'):
             id = self.request.get('id')
-            #response = requests.get("https://bdr-test.eionet.europa.eu/_cache/undertaking/detail/" + str(id), verify=False)
-            response = requests.get("http://eftimie.ro/fapi/undertaking/list")
+            response = requests.get("https://bdr-test.eionet.europa.eu/_cache/undertaking/detail/" + str(id), verify=False)
             if response.status_code == requests.codes.ok:
-                return response.json()[int(id) - 1]
+                return response.json()
         return {}
