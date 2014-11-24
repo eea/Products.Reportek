eeef500f492ec7b6c0e29351bd3d180fd8eff446
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2014-11-24 18:35:43 +0200

    Make call robust when no Satelite Registry exists

--- Products/Reportek/views/satellite_registry_management.py
+++ Products/Reportek/views/satellite_registry_management.py
@@ -7,7 +7,10 @@ class SatelliteRegistryManagement(BaseAdmin):
     def __call__(self, *args, **kwargs):
         super(SatelliteRegistryManagement, self).__call__(*args, **kwargs)
         if self.request.method == "POST" and self.request.get("verify.btn"):
-            api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi.authMiddlewareApi
+            api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi
+            if not api:
+                return None
+            api = api.authMiddlewareApi
             candidateId = self.request.get("cid")
             newId = self.request.get("fid")
             if candidateId and newId:
@@ -17,7 +20,10 @@ class SatelliteRegistryManagement(BaseAdmin):
         return self.index()
 
     def get_companies(self):
-        api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi.authMiddlewareApi
+        api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi
+        if not api:
+            return None
+        api = api.authMiddlewareApi
         return api.getCompanies()
 
     def get_company_alldetails(self):
@@ -31,11 +37,17 @@ class SatelliteRegistryManagement(BaseAdmin):
 
     def get_company_details(self):
         if self.request.get('id'):
-            api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi.authMiddlewareApi
+            api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi
+            if not api:
+                return None
+            api = api.authMiddlewareApi
             companyId = self.request.get('id')
             return api.getCompanyDetails(companyId)
 
 
     def get_candidates(self):
-        api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi.authMiddlewareApi
+        api = self.context.unrestrictedTraverse('/'+ENGINE_ID).authMiddlewareApi
+        if not api:
+            return None
+        api = api.authMiddlewareApi
         return api.getCandidates()
