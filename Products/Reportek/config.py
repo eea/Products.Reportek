9ec10be2e515a3acecf8948357101f1208c37d50
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2015-01-05 13:50:35 +0200

    Better deployent separation
    Conflicts:
    	Products/Reportek/config.py

--- Products/Reportek/config.py
+++ Products/Reportek/config.py
@@ -15,27 +15,28 @@
 import os
 
 __all__ = [
-    'REPORTEK_DEPLOYMENT',
     'DEPLOYMENT_BDR',
     'DEPLOYMENT_CDR',
+    'DEPLOYMENT_MDR',
     'permission_manage_properties_collections',
     'permission_manage_properties_envelopes',
     'LOCAL_CONVERTERS_PORT',
-    'REDIS_DATABASE',
 ]
 
 DEPLOYMENT_CDR = 'CDR'
 DEPLOYMENT_BDR = 'BDR'
+DEPOLYMENT_MDR = 'MDR'
 
 REPORTEK_DEPLOYMENT = os.environ.get('REPORTEK_DEPLOYMENT', DEPLOYMENT_CDR)
 LOCAL_CONVERTERS_PORT = os.environ.get('LOCAL_CONVERTERS_PORT', '5000')
 
-if REPORTEK_DEPLOYMENT == DEPLOYMENT_CDR:
+if REPORTEK_DEPLOYMENT in (DEPLOYMENT_CDR, DEPOLYMENT_MDR):
     permission_manage_properties_collections = 'Change Collections'
     permission_manage_properties_envelopes = 'Change Envelopes'
 elif REPORTEK_DEPLOYMENT == DEPLOYMENT_BDR:
     permission_manage_properties_collections = 'Manage properties'
     permission_manage_properties_envelopes = 'Manage properties'
 
-REDIS_DATABASE = os.environ.get('REDIS_DATABASE', '0')
-REDIS_DATABASE = int(REDIS_DATABASE)
+if REPORTEK_DEPLOYMENT == DEPLOYMENT_CDR:
+    REDIS_DATABASE = int(os.environ.get('REDIS_DATABASE', '0'))
+    __all__.append('REDIS_DATABASE')
