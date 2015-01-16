ca85316f43bbfdcd108dce3f17b3cc1f98023216
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2015-01-16 14:32:33 +0200

    Refs #22535 stay robust while middleware is down

--- Products/Reportek/BdrAuthorizationMiddlewareApi.py
+++ Products/Reportek/BdrAuthorizationMiddlewareApi.py
@@ -19,7 +19,11 @@ class AuthMiddlewareApi(object):
     def getCollectionPaths(self, username):
         url = self.baseUrl + '/user/' + username + '/companies'
         # use a short timeout here to not keep the user waiting at auth time
-        response = requests.get(url, timeout=self.TIMEOUT)
+        try:
+            response = requests.get(url, timeout=self.TIMEOUT)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return []
         if response.status_code == 404:
             return []
         if response.status_code != requests.codes.ok:
@@ -39,16 +43,24 @@ class AuthMiddlewareApi(object):
         return paths
 
     def getCompanies(self):
-        response = requests.get(self.baseUrl + "/undertaking/list",
+        try:
+            response = requests.get(self.baseUrl + "/undertaking/list",
                                 timeout=self.TIMEOUT, verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
         return response.json()
 
     def existsCompany(self, params):
-        response = requests.get(self.baseUrl + "/undertaking/filter/",
+        try:
+            response = requests.get(self.baseUrl + "/undertaking/filter/",
                                 timeout=self.TIMEOUT, verify=False,
                                 params=params)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
         return response.json()
@@ -60,10 +72,14 @@ class AuthMiddlewareApi(object):
             api_url = "/candidate/verify/{0}/{1}/"
 
         api_url = api_url.format(companyId, candidateId)
-        response = requests.post(self.baseUrl + api_url,
+        try:
+            response = requests.post(self.baseUrl + api_url,
                                  data={'user': userId},
                                  timeout=self.TIMEOUT,
                                  verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return False
         if response.status_code == requests.codes.ok:
             data = response.json()
             if 'verified' in data and data['verified']:
@@ -71,15 +87,23 @@ class AuthMiddlewareApi(object):
         return False
 
     def getCandidates(self):
-        response = requests.get(self.baseUrl + "/candidate/list",
+        try:
+            response = requests.get(self.baseUrl + "/candidate/list",
                                 timeout=self.TIMEOUT, verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
         return response.json()
 
     def getCompanyDetailsById(self, companyId):
-        response = requests.get(self.baseUrl + "/undertaking/{0}/details".format(companyId),
+        try:
+            response = requests.get(self.baseUrl + "/undertaking/{0}/details".format(companyId),
                                 timeout=self.TIMEOUT, verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
 
@@ -98,15 +122,23 @@ class AuthMiddlewareApi(object):
         return details
 
     def getMatchingLog(self):
-        response = requests.get(self.baseUrl + "/matching_log",
+        try:
+            response = requests.get(self.baseUrl + "/matching_log",
                                 timeout=self.TIMEOUT, verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
         return response.json()
 
     def getDataSyncLog(self):
-        response = requests.get(self.baseUrl + "/data_sync_log",
+        try:
+            response = requests.get(self.baseUrl + "/data_sync_log",
                                 timeout=self.TIMEOUT, verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
         return response.json()
@@ -119,8 +151,12 @@ class AuthMiddlewareApi(object):
         return response
 
     def getUsersExcelExport(self):
-        response = requests.get(self.baseUrl + "/misc/user/export",
+        try:
+            response = requests.get(self.baseUrl + "/misc/user/export",
                                 timeout=self.TIMEOUT, verify=False)
+        except Exception as e:
+            logger.warning("Error contacting SatelliteRegistry (%s)" % str(e))
+            return None
         if response.status_code != requests.codes.ok:
             return None
         return response
