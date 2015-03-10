15a9fb0e9080b607af849da1cb1d1bb0ae5608de
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2015-03-10 11:09:16 +0200

    move getCountryName and getCountryCode to Reportek Collection

--- Products/Reportek/Envelope.py
+++ Products/Reportek/Envelope.py
@@ -1242,29 +1242,6 @@ class Envelope(EnvelopeInstance, EnvelopeRemoteServicesManager, EnvelopeCustomDa
     def has_permission(self, *args, **kwargs):
         return self.acquiredRolesAreUsedBy(*args, **kwargs)
 
-    def getCountryName(self, country_uri=None):
-        """ Returns country name from the country uri
-        """
-        dummycounty = {'name':'Unknown', 'iso': 'xx'}
-        eng = getattr(self, ENGINE_ID)
-        if country_uri:
-            try:
-                return str([x['name'] for x in eng.localities_table() if str(x['uri']) == country_uri][0])
-            except:
-                return dummycounty['name']
-        return str(eng.localities_dict().get(self.country, dummycounty)['name'])
-
-    def getCountryCode(self, country_uri=None):
-        """ Returns country ISO code from the country uri
-        """
-        dummycounty = {'name':'Unknown', 'iso': 'xx'}
-        eng = getattr(self, ENGINE_ID)
-        if country_uri:
-            try:
-                return str([x['iso'] for x in eng.localities_table() if str(x['uri']) == country_uri][0])
-            except:
-                return dummycounty['iso']
-        return str(eng.localities_dict().get(self.country, dummycounty)['iso'])
 
 # Initialize the class in order the security assertions be taken into account
 Globals.InitializeClass(Envelope)
