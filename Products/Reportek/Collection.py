20ffac89f0130d6f6df857e9df0fa04d2d0078ff
Olimpiu Rob <olimpiu.rob@gmail.com>
2015-07-16 11:56:00 +0300

    Refs #27232; Added member type detection in 'get_users_list' view

--- Products/Reportek/Collection.py
+++ Products/Reportek/Collection.py
@@ -218,42 +218,64 @@ class Collection(CatalogAware, Folder, Toolz):
         """ List accounts with the reporter and client roles for current folder and subfolders """
         from ldap.dn import explode_dn
         role_param = REQUEST.get('role', '')
-        users = {}
-        global_users = {}
+        members = {}
+        global_members = {}
         catalog = getattr(self, constants.DEFAULT_CATALOG)
+        ldap_groups = self.getLDAPGroups()
         # retrieve the global accounts
         ldap_user_folder = self.acl_users['ldapmultiplugin']['acl_users']
         for user_dn, roles in ldap_user_folder.getLocalUsers():
-            user = explode_dn(user_dn,notypes=1)[0]
+            member = explode_dn(user_dn,notypes=1)[0]
             for role in roles:
                 if role_param and role_param in ['Reporter', 'Client']:
                     if role == role_param:
-                        global_users[user] =[role]
+                        global_members[member] = {
+                            'type': 'user',
+                            'roles': [role]
+                        }
                 else:
                     if role in ['Reporter', 'Client']:
-                        if global_users.has_key(user):
-                            global_users[user].append(role)
+                        if member in global_members:
+                            global_members[member]['roles'].append(role)
                         else:
-                            global_users[user] = [role]
+                            global_members[member] = {
+                                'type': 'user',
+                                'roles': [role]
+                            }
         # retrieve the local accounts
         folders = catalog(meta_type=['Report Collection'], path=self.absolute_url(1))
         for folder in folders:
             context = catalog.getobject(folder.data_record_id_)
-            for user, roles in context.get_local_roles():
+            for member, roles in context.get_local_roles():
                 for role in list(roles):
                     if role_param and role_param in ['Reporter', 'Client']:
                         if role == role_param:
-                            if users.has_key(user):
-                                users[user].append([context, [role]])
+                            if member in members:
+                                members[member]['roles'].append([context, [role]])
                             else:
-                                users[user] = [[context, [role]]]
+                                u_type = 'user'
+                                if member in ldap_groups:
+                                    u_type = 'group'
+                                members[member] = {
+                                    'type': u_type,
+                                    'roles': [[context, [role]]]
+                                }
                     else:
                         if role in ['Reporter', 'Client']:
-                            if users.has_key(user):
-                                users[user].append([context, list(roles)])
+                            if member in members:
+                                members[member]['roles'].append([context, list(roles)])
                             else:
-                                users[user] = [[context, list(roles)]]
-        return self._get_users_list(REQUEST, users=users, global_users=global_users)
+                                u_type = 'user'
+                                if member in ldap_groups:
+                                    u_type = 'group'
+                                members[member] = {
+                                    'type': u_type,
+                                    'roles': [[context, list(roles)]]
+                                }
+
+        return self._get_users_list(REQUEST, members=members,
+                                    global_members=global_members,
+                                    groups=bool(ldap_groups))
 
     security.declarePublic('years')
     def years(self):
