d7fc4ee50655f0c4a3f2a15b04bfdc57ac495df2
Olimpiu Rob <olimpiu.rob@gmail.com>
2016-11-09 17:58:38 +0200

    Refs #78603; Fixed update script for QARepository object

--- Products/Reportek/updates/u20161108_migrate_QARepository_attributes.py
+++ Products/Reportek/updates/u20161108_migrate_QARepository_attributes.py
@@ -7,9 +7,8 @@ from Products.Reportek.updates import MigrationBase
 from Products.Reportek.config import DEPLOYMENT_CDR
 from Products.Reportek.config import DEPLOYMENT_MDR
 from Products.Reportek.config import DEPLOYMENT_BDR
-from Products.Reportek.constants import DEFAULT_CATALOG
+from Products.Reportek.constants import QAREPOSITORY_ID
 import logging
-import transaction
 
 logger = logging.getLogger(__name__)
 VERSION = 11
@@ -21,29 +20,11 @@ APPLIES_TO = [
 
 
 def migrate_QARepository_attributes(app):
-    catalog = app.unrestrictedTraverse('/' + DEFAULT_CATALOG)
-    brains = catalog({'meta_type': 'Reportek QARepository'})
-
-    count = 0
-    for brain in brains:
-        try:
-            obj = brain.getObject()
-        except Exception as e:
-            logger.error('Unable to retrieve object: {} due to {}'.format(brain.getURL(), str(e)))
-        if obj:
-            do_update = False
-            if not hasattr(obj, 'QA_application'):
-                obj.QA_application = 'EnvelopeQAApplication'
-                do_update = True
-            if do_update:
-                obj.reindex_object()
-
-            if count % 10000 == 0:
-                transaction.savepoint()
-                logger.info('savepoint at %d records', count)
-
-            count += 1
-
+    qa_repo = app.unrestrictedTraverse('/' + QAREPOSITORY_ID)
+    if hasattr(qa_repo, 'setstate'):
+        del qa_repo.setstate
+        qa_repo._p_changed = 1
+        logger.info('Changed attributes for QARepository')
     return True
 
 
