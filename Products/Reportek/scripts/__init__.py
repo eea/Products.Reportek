5eeeb93d6dce937b2124eeb4b47fc6e7443d388b
Olimpiu Rob <olimpiu.rob@gmail.com>
2017-08-28 17:44:13 +0300

    Refs #87250; Added cleanup_zip_cache and automatic_qa console_scripts

---
+++ Products/Reportek/scripts/__init__.py
@@ -0,0 +1,21 @@
+""" Script utilities
+"""
+import os
+HOST = os.environ.get('DEPLOYMENT_HOST', 'localhost')
+
+
+def get_zope_site():
+    import Zope2
+    app = Zope2.app()
+    from Testing.ZopeTestCase import utils
+    utils._Z2HOST = HOST
+    app = utils.makerequest(app)
+    app.REQUEST['PARENTS'] = [app]
+    from zope.globalrequest import setRequest
+    setRequest(app.REQUEST)
+
+    from AccessControl.SpecialUsers import system as user
+    from AccessControl.SecurityManagement import newSecurityManager
+    newSecurityManager(None, user)
+
+    return app
