8b44421abd7f1d3919fa3d2deec1a2e5e7627337
Olimpiu Rob <olimpiu.rob@gmail.com>
2018-06-07 16:29:05 +0300

    Refs #94525; Updated auto_falling script with the following changes:
    - added support to pass multiple obligations
    - added workflow argument
    - added checks for act_to validity and envelope workflow

--- Products/Reportek/scripts/auto_fallin.py
+++ Products/Reportek/scripts/auto_fallin.py
@@ -37,37 +37,43 @@ def main():
     bin/instance run bin/auto_fallin --obligation <obligation_id> --act_from <ActivityID> --act_to <ActivityID> --env_year 2017
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--obligation',
-                        help="Obligation, e.g. 673 or http://rod.eionet.europa.eu/obligations/673",
-                        dest='obligation')
+    parser.add_argument('--obligation(s)',
+                        help='Obligation(s), e.g. 673 or http://rod.eionet.europa.eu/obligations/673',
+                        dest='obligations',
+                        nargs = '*')
     parser.add_argument('--act_from',
-                        help="Activity from which to forward, e.g. FinalFeedback",
+                        help='Activity from which to forward, e.g. FinalFeedback',
                         dest='act_from')
     parser.add_argument('--act_to',
-                        help="Activity to which to forward, e.g. TechnicallyAccepted",
+                        help='Activity to which to forward, e.g. TechnicallyAccepted',
                         dest='act_to')
+    parser.add_argument('--workflow',
+                        help='Workflow of the envelopes',
+                        dest='act_wf')
     parser.add_argument('--env_year',
-                        help="Envelope year, e.g. 2017, default=current year",
+                        help='Envelope year, e.g. 2017, default=current year',
                         dest='env_year',
                         default=DateTime().year())
     args = parser.parse_args(sys.argv[3:])
-    if (args.obligation == None or args.act_from == None or args.act_to == None):
+    if (args.obligations == None or args.act_from == None or args.act_to == None) or args.act_wf == None:
         parser.print_help()
         sys.exit()
-    obl = args.obligation
-    if not obl.startswith('http'):
-        df_prefix = 'http://rod.eionet.europa.eu/obligations/{}'
-        obl = df_prefix.format(obl)
+    obls = args.obligations
+    df_prefix = 'http://rod.eionet.europa.eu/obligations/{}'
+    obls = [df_prefix.format(obl) if not obl.startswith('http') else obl
+            for obl in obls]
     site = get_zope_site()
     catalog = site.unrestrictedTraverse(DEFAULT_CATALOG, None)
-    results = get_envelopes(catalog, obl, args.act_from, args.act_to)
+    results = get_envelopes(catalog, obls, args.act_from, args.act_to)
     savepoint = transaction.savepoint()
     try:
         for key, value in results.iteritems():
             entry_savepoint = transaction.savepoint()
             wk = value.get('wk')
             env = value.get('envelope')
-            if str(env.year) == str(args.env_year):
+            env_wf = env.getProcess()
+            activity = getattr(env_wf, args.act_to, None)
+            if str(env.year) == str(args.env_year) and args.act_wf == env_wf.id and activity:
                 try:
                     env.falloutWorkitem(wk.id)
                     env.fallinWorkitem(wk.id, args.act_to)
