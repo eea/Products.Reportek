405d58e2d2b8d2a88b94dd0cd49da1f959725d65
Olimpiu Rob <olimpiu.rob@gmail.com>
2018-06-12 17:20:37 +0300

    Added SCHEDULE_START debug info

--- Products/Reportek/scripts/auto_fallin.py
+++ Products/Reportek/scripts/auto_fallin.py
@@ -27,6 +27,7 @@ except Exception:
 now = DateTime()
 should_run = False
 print "Now: {}".format(now.HTML4())
+print "Scheduled start: {}".format(SCHEDULE_START.HTML4())
 if SCHEDULE_PERIOD == 'weekly':
     if int(math.ceil(now - SCHEDULE_START)) % 7 == 0:
         should_run = True
