42be7ac992874e7324de5d2872747d6fe08d104f
Olimpiu Rob <olimpiu.rob@gmail.com>
2018-09-04 13:49:08 +0300

    Refs #97725; Explicitly commit the transaction after adding a AQA Report Feedback

--- Products/Reportek/RemoteApplication.py
+++ Products/Reportek/RemoteApplication.py
@@ -495,7 +495,7 @@ class RemoteApplication(SimpleItem):
 
                 feedback_ob.message = l_ret.get('FEEDBACK_MESSAGE', '')
                 l_workitem.addEvent("Added Automatic QA feedback file: {}".format(feedback_id))
-                # Make a savepoint after adding the Feedback
+                # Explicitly commit the transaction after adding the Feedback
                 transaction.savepoint()
                 l_getResultDict = {p_jobID: {'code':1, 'fileURL':l_file_url}}
                 self.__manageAutomaticProperty(p_workitem_id=p_workitem_id, p_getResult=l_getResultDict)
