5f8c43b01d7c62c06d26fb6b101fb409378c4527
Olimpiu Rob <olimpiu.rob@gmail.com>
2019-09-05 14:27:51 +0300

    Refs #104682; Refactored BadRequestErrorView to handle Unauthorized and NotFound as well

--- Products/Reportek/errors/__init__.py
+++ Products/Reportek/errors/__init__.py
@@ -3,14 +3,20 @@ import json
 import re
 
 
-class BadRequestErrorView(object):
-    """View rendered on BadRequestError."""
+class ErrorView(object):
+    """View rendered on some errors."""
 
     def __call__(self, *args, **kwargs):
         accept = self.request.environ.get("HTTP_ACCEPT")
-        error_type = 'BadRequest'
+        error_type = self.context.__class__.__name__
         if accept == 'application/json':
-            self.request.RESPONSE.setHeader('Content-Type', 'application/json')
+            status = {
+                'Unauthorized': 401,
+                'NotFound': 404,
+                'BadRequest': 400
+            }
+            self.request.response.setHeader('Content-Type', 'application/json')
+            self.request.response.setStatus(status.get(error_type))
             error = {
                 'title': error_type,
                 'description': re.sub('<[^<]+?>', '', self.context.message)
@@ -18,10 +24,9 @@ class BadRequestErrorView(object):
             data = {
                 'errors': [error],
             }
-            return json.dumps(data, indent=4)
+            return self.request.response.write(json.dumps(data, indent=4))
 
         ctx = aq_parent(self)
         return ctx.standard_error_message(ctx, self.request,
                                           error_type=error_type,
                                           error_value=self.context.message)
-
