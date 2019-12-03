b6b741a1ab44738bf4ed57340426193b6f4b35d5
Olimpiu Rob <olimpiu.rob@gmail.com>
2019-12-03 11:00:01 +0200

    Refs #109761; Added missing utils_forwardable_envelopes.js and envelopes_forwardable.zpt

---
+++ Products/Reportek/www/static/utils_forwardable_envelopes.js
@@ -0,0 +1,107 @@
+/*global $*/
+"use strict";
+if (window.reportek === undefined) {
+  var reportek = {
+    version: "1.0",
+    utils: {}
+  };
+}
+
+reportek.utils.forwardable_envelopes = {
+  load: function() {
+    var self = reportek.utils.forwardable_envelopes;
+    self.bind_tabs();
+    self.init_table();
+    self.bind_age_controls();
+  },
+
+  bind_tabs: function() {
+    var self = reportek.utils.forwardable_envelopes;
+    $(".ajaxtabsmenu a").on("click", function() {
+      self.update_table_values();
+    });
+  },
+
+  bind_age_controls: function() {
+    var self = reportek.utils.forwardable_envelopes;
+    function isInt(n) {
+        return +n === n && !(n % 1);
+    }
+    var age = 30;
+    $("#age").on('keyup', function (e) {
+      if (e.keyCode == 13) {
+        age = parseInt($(this).val());
+        if (isInt(age) && age > 0) {
+          self.update_table_values();
+        } else {
+          alert("Please enter the number of days!")
+          return;
+        }
+      }
+    });
+    $("input[name='update']").on("click", function() {
+      age = parseInt($("#age").val());
+      if (isInt(age) && age > 0) {
+        self.update_table_values();
+        $("#age_info").text($("#age").val());
+      } else {
+          alert("Please enter the number of days!")
+          return;
+      }
+    });
+  },
+
+  init_table() {
+    var self = reportek.utils.forwardable_envelopes;
+    $("#f_envs").DataTable({
+      "iDisplayLength": 50,
+      "bProcessing": true,
+      "ajax": {
+        "url": "get_forwardable_envelopes",
+        "type": "GET",
+        "dataSrc": ""
+      },
+      "bAutoWidth": false,
+      "order": [[ 1, "asc" ]],
+      "columnDefs": [
+        {
+          "targets": 0,
+          "render": function (data, type, full, meta) {
+            return '<input type="checkbox" name="envelopes:list" class="toggledCB" value="' + full.env.url + '">';
+          }
+        },
+        {
+          "targets": 1,
+          "render": function (data, type, full, meta) {
+            return '<a href="' + full.env.url + '">' + full.env.path + '</a>';
+          }
+        },
+        {
+          "targets": 2,
+          "render": function (data, type, full, meta) {
+            return '<a href="' + full.process.url + '">' + full.process.title + '</a>';
+          }
+        },
+        {
+          "targets": 3,
+          "render": function (data, type, full, meta) {
+            return '<a href="' + full.activity.url + '">' + full.activity.title + '</a>';
+          }
+        },
+        {
+          "targets": 4,
+          "data": "s_date"
+        },
+      ]
+    });
+  },
+  update_table_values: function() {
+    var self = reportek.utils.forwardable_envelopes;
+    var table = $("#s_envs").DataTable();
+    table.ajax.url(self.get_data_url()).load();
+  }
+};
+
+$(document).ready(function () {
+  reportek.utils.forwardable_envelopes.load();
+});
