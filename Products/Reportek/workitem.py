b31299ca12a1b983d9c6617224fdcab00c8b261b
Olimpiu Rob <olimpiu.rob@gmail.com>
2021-04-12 10:29:38 +0300

    Refs #130945; auto PEP8 in workitem

--- Products/Reportek/workitem.py
+++ Products/Reportek/workitem.py
@@ -1,4 +1,5 @@
 from time import time
+import string
 
 import constants
 from AccessControl import ClassSecurityInfo
@@ -22,7 +23,8 @@ def computed_attribute_decorator(level=0):
     return computed_attribute_wrapper
 
 
-class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAware):
+class workitem(CatalogAware, object, SimpleItem, PropertyManager,
+               DFlowCatalogAware):
     """ describes a single workitem of the history graph """
 
     meta_type = 'Workitem'
@@ -35,69 +37,80 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
 
     def __init__(self, id, instance_id, activity_id, blocked,
                  priority=0, workitems_from=[], workitems_to=[],
-                 push_roles=[], pull_roles=[], blocker=False) :
+                 push_roles=[], pull_roles=[], blocker=False):
         self.id = id
         self.activity_id = activity_id
         self.instance_id = instance_id
         self.workitems_from = workitems_from[:]
         self.workitems_to = workitems_to[:]
         self.status = (blocked and 'blocked') or 'inactive'
-        #possible states: blocked,inactive,active,suspended,fallout,complete
+        # possible states: blocked,inactive,active,suspended,fallout,complete
         self.event_log = []
         self.actor = ''
         self.graph_level = 0
         self.priority = priority
         self.blocked = blocked
-        #blocker means the envelope can not be released due to errors in feedback
+        # blocker means the envelope can not be released due to errors
+        # in feedback
         self.blocker = blocker
         self.push_roles = push_roles
         self.pull_roles = pull_roles
-        #logging structure
-        #each event has the form {'start':xxx, 'end':yyy, 'comment':zzz, 'actor':aaa}
-        #'start' and 'end' are time in msec, 'actor' and 'comment' are string
+        # logging structure
+        # each event has the form {'start':xxx, 'end':yyy, 'comment':zzz,
+        # 'actor':aaa}
+        # 'start' and 'end' are time in msec, 'actor' and 'comment' are string
         self.inactivation_log = []
         self.activation_log = []
         self.completion_log = []
         self.suspension_log = []
         self.fallout_log = []
-        #statistic data in msec
+        # statistic data in msec
         self.inactive_time = 0
         self.active_time = 0
         self.suspended_time = 0
         self.fallout_time = 0
         self.creation_time = DateTime()
-        #log initialization
-        if self.status=='inactive':
-            self.inactivation_log.append({'start':time(),'end':None,'comment':'creation','actor':''})
+        # log initialization
+        if self.status == 'inactive':
+            self.inactivation_log.append(
+                {'start': time(), 'end': None, 'comment': 'creation',
+                 'actor': ''})
 
-    manage_options=(
-        ( {'label':'Properties', 'action':'workitemProperties', 'help' : ('OpenFlowBase', 'workitems.stx') }, ) +
-        ( {'label':'View', 'action':'index_html' }, ) +
+    manage_options = (
+        ({'label': 'Properties', 'action': 'workitemProperties',
+          'help': ('OpenFlowBase', 'workitems.stx')}, ) +
+        ({'label': 'View', 'action': 'index_html'}, ) +
         SimpleItem.manage_options
-        )
+    )
 
     security.declareProtected('Manage OpenFlow', 'workitemProperties')
-    workitemProperties = PageTemplateFile('zpt/Workflow/workitem_edit', globals())
+    workitemProperties = PageTemplateFile(
+        'zpt/Workflow/workitem_edit', globals())
 
     security.declareProtected('View', 'workitemDetails')
-    workitemDetails = PageTemplateFile('zpt/Workflow/workitem_details', globals())
+    workitemDetails = PageTemplateFile(
+        'zpt/Workflow/workitem_details', globals())
 
     security.declareProtected('View', 'index_html')
     index_html = PageTemplateFile('zpt/Workflow/workitem_index', globals())
 
     def title(self):
-        return self.activity_id + " by " + self.actor + ", status: " + self.status
+        return self.activity_id + " by " + self.actor + ", status: " +\
+               self.status
 
     def activity_application(self, activity_id):
         activity = getattr(self.getProcess(), activity_id, None)
         wf_engine = getattr(self, constants.WORKFLOW_ENGINE_ID)
-        apps_folder = getattr(self.getPhysicalRoot(), constants.APPLICATIONS_FOLDER_ID)
+        apps_folder = getattr(self.getPhysicalRoot(),
+                              constants.APPLICATIONS_FOLDER_ID)
         path = ''
         if activity:
             if activity.application:
-                path = wf_engine._applications.get(activity.application, {}).get('url')
+                path = wf_engine._applications.get(
+                    activity.application, {}).get('url')
             elif apps_folder.get(self.getProcess().id, {}).get(activity.id):
-                path = apps_folder[self.getProcess().id][activity.id].virtual_url_path()
+                path = apps_folder[self.getProcess(
+                ).id][activity.id].virtual_url_path()
             elif apps_folder.get('Common', {}).get(activity.id):
                 path = apps_folder['Common'][activity.id].virtual_url_path()
             app = self.getPhysicalRoot().restrictedTraverse(path)
@@ -106,8 +119,9 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
                 manage_page = 'manage_settings_html'
         url = "-"
         if path:
-            url = '/%s/%s' %(path, manage_page)
-        return {'id': getattr(activity, 'application', None) or activity_id, 'url': url}
+            url = '/%s/%s' % (path, manage_page)
+        return {'id': getattr(activity, 'application', None) or activity_id,
+                'url': url}
 
     def activity_link(self, activity_id):
         activity = getattr(self.getProcess(), activity_id, None)
@@ -115,55 +129,58 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
         return {'id': activity_id, 'url': url}
 
     def lastActivityDate(self):
-        """ Returns last activity date and time based on the workitem's event log """
+        """ Returns last activity date and time based on the workitem's
+            event log
+        """
         if len(self.event_log):
             return self.event_log[-1]['time']
         return self.creation_time
 
     security.declareProtected('Manage OpenFlow', 'edit')
+
     def edit(self,
-             instance_id = None,
-             activity_id = None,
-             blocked = None,
-             priority = None,
-             workitems_from = None,
-             workitems_to = None,
-             status = None,
-             actor = None,
-             graph_level = None):
+             instance_id=None,
+             activity_id=None,
+             blocked=None,
+             priority=None,
+             workitems_from=None,
+             workitems_to=None,
+             status=None,
+             actor=None,
+             graph_level=None):
         """ Edit Workitem's properties """
-        if instance_id != None:
+        if instance_id is not None:
             self.instance_id = instance_id
-        if activity_id != None:
+        if activity_id is not None:
             self.activity_id = activity_id
-        if priority != None:
+        if priority is not None:
             self.priority = priority
-        if workitems_from != None:
+        if workitems_from is not None:
             self.workitems_from = workitems_from
-        if workitems_to != None:
+        if workitems_to is not None:
             self.workitems_to = workitems_to
-        if status != None:
+        if status is not None:
             self.setStatus(status)
-        if blocked != None:
+        if blocked is not None:
             self.blocked = blocked
             if blocked:
-                self.status='blocked'
-        if actor != None:
+                self.status = 'blocked'
+        if actor is not None:
             self.actor = actor
-        if graph_level != None:
+        if graph_level is not None:
             self.graph_level = graph_level
         self.reindex_object()
 
     def addFrom(self, id):
         """ """
         self.workitems_from.append(id)
-        self._p_changed=1
+        self._p_changed = 1
         self.reindex_object()
 
     def addTo(self, id_list):
         """ """
         self.workitems_to.extend(id_list)
-        self._p_changed=1
+        self._p_changed = 1
         self.reindex_object()
 
     def setGraphLevel(self, graph_level):
@@ -173,22 +190,27 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
 
     def addEvent(self, event, comment=''):
         """ """
-        self.event_log.append({'event' : event, 'time' : DateTime(), 'comment': comment})
-        self._p_changed=1
+        self.event_log.append(
+            {'event': event, 'time': DateTime(), 'comment': comment})
+        self._p_changed = 1
         notify(ObjectModifiedEvent(self))
 
     security.declareProtected('View management screens', 'addEventOnTime')
+
     def addEventOnTime(self, event, p_time, comment=''):
-        """ Used only by Managers when updating an workitem log from records that already exist """
+        """ Used only by Managers when updating an workitem log from records
+            that already exist
+        """
         p_time = DateTime(p_time)
-        self.event_log.append({'event' : event, 'time' : p_time, 'comment': comment})
-        self._p_changed=1
+        self.event_log.append(
+            {'event': event, 'time': p_time, 'comment': comment})
+        self._p_changed = 1
 
     def isActiveOrInactiveOn(self, process_id, activity_id):
         """ """
         return (self.status in ('active', 'inactive')) \
-               and string.split(self.process_path, '/')[-1] == process_id \
-               and self.activity_id == activity_id
+            and string.split(self.process_path, '/')[-1] == process_id \
+            and self.activity_id == activity_id
 
     def unblock(self, value=1, comment='', actor=''):
         """ """
@@ -197,10 +219,11 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
             self.setStatus('inactive', comment, actor)
 
     security.declareProtected('View management screens', 'set_blocker_attr')
+
     def set_blocker_attr(self, value=1, comment='', actor=''):
         """ sets the 'blocker' flag at the specified value
-            used by Managers to fix the envelopes in which the automatic QA failed 
-            and their acceptability status is wrong
+            used by Managers to fix the envelopes in which the automatic
+            QA failed and their acceptability status is wrong
         """
         if value:
             self.blocker = 1
@@ -218,24 +241,30 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
         new_status = status
         now = time()
 
-        logs = {'inactive':self.inactivation_log,
-                'active':self.activation_log,
-                'suspended':self.suspension_log,
-                'fallout':self.fallout_log,
-                'complete':self.completion_log}
-        if logs.has_key(old_status) and logs[old_status]:
+        logs = {'inactive': self.inactivation_log,
+                'active': self.activation_log,
+                'suspended': self.suspension_log,
+                'fallout': self.fallout_log,
+                'complete': self.completion_log}
+        if old_status in logs and logs[old_status]:
             # sets end to old status log
             logs[old_status][-1]['end'] = now
             # adds appropriate time interval to time tracking attribute
             x = now - logs[old_status][-1]['start']
-            if old_status=='inactive': self.inactive_time += x
-            elif old_status=='active': self.active_time += x
-            elif old_status=='suspended': self.suspended_time += x
-            elif old_status=='fallout': self.fallout_time += x
-        if logs.has_key(new_status):
+            if old_status == 'inactive':
+                self.inactive_time += x
+            elif old_status == 'active':
+                self.active_time += x
+            elif old_status == 'suspended':
+                self.suspended_time += x
+            elif old_status == 'fallout':
+                self.fallout_time += x
+        if new_status in logs:
             # creates new element to new status log
-            logs[new_status].append({'start':now,'end':None,'comment':comment,'actor':actor})
-            
+            logs[new_status].append(
+                {'start': now, 'end': None,
+                 'comment': comment, 'actor': actor})
+
         self.status = status
         self.actor = actor
         self.addEvent(status, comment)
@@ -267,7 +296,7 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
         l_process = self.unrestrictedTraverse(self.process_path, None)
         try:
             return getattr(getattr(l_process, self.activity_id), p_attribute)
-        except:
+        except Exception:
             return self.activity_id
 
     def getActivityAttribute(self, attr):
@@ -276,7 +305,7 @@ class workitem(CatalogAware, object, SimpleItem, PropertyManager, DFlowCatalogAw
         process = self.unrestrictedTraverse(self.process_path, None)
         try:
             return getattr(getattr(process, self.activity_id), attr)
-        except:
+        except Exception:
             return None
 
     @property
