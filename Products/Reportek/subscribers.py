7f4cd0149367286b93da4d7312eae6aa62f03520
Olimpiu Rob <olimpiu.rob@gmail.com>
2022-03-31 17:29:26 +0300

    Refs #148261; Added event subscriber to update data_file mtime on rename

--- Products/Reportek/subscribers.py
+++ Products/Reportek/subscribers.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from DateTime import DateTime
+from time import time
 
 
 def handle_document_removed_event(obj, event):
@@ -17,3 +18,9 @@ def handle_feedback_added_event(obj, event):
     env.last_fb = DateTime()
     env._p_changed = 1
     env.reindex_object()
+
+def handle_document_renamed_event(obj, event):
+    """Force the update of data_file's mtime value"""
+    obj.data_file.mtime = time()
+    obj._p_changed = 1
+    obj.reindex_object()
