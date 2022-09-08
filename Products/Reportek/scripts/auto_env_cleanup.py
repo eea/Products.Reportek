d871c53160a402c5c62c13adb150735c70570f0d
Olimpiu Rob <olimpiu.rob@gmail.com>
2022-09-08 17:30:44 +0300

    fix(scripts): loop over brains instead of objects - refs #139601

--- Products/Reportek/scripts/auto_env_cleanup.py
+++ Products/Reportek/scripts/auto_env_cleanup.py
@@ -29,55 +29,72 @@ def get_envelopes(catalog, inactive_for, limit):
 
     brains = searchResults(catalog, query)
 
-    return [brain.getObject() for brain in brains]
+    return brains
 
 
 def do_cleanup(site, inactive_for=30, limit=None):
     catalog = site.Catalog
     engine = site.ReportekEngine
-    envs = get_envelopes(catalog, inactive_for, limit)
+    b_envs = get_envelopes(catalog, inactive_for, limit)
     env_count = 0
     col_count = 0
-    for env in envs:
+    cols_removed = []
+    processed = []
+    envs_removed = []
+    for b in b_envs:
+        try:
+            env = b.getObject()
+        except Exception as e:
+            print "Unable to retrieve envelope object"
+        processed.append(env.absolute_url())
         col = env.getParentNode()
-        terminated = [df for df in col.dataflow_uris
-                      if engine.dataflow_lookup(df).get('terminated') == '1']
-        if terminated and len(terminated) == len(col.dataflow_uris):
-            print "Removing {}. Terminated obligations: {}".format(
-                    col.absolute_url(),
-                    terminated)
-            col_par = col.getParentNode()
-            changed = False
-            if not getattr(col_par, 'can_move_released', False):
-                col_par.can_move_released = True
-                changed = True
-            print "Parent collection: {}".format(col_par.absolute_url())
-            try:
-                col_par.manage_delObjects(col.getId())
-                if changed:
-                    del col_par.can_move_released
-                col_count += 1
-            except Exception as e:
-                print "Unable to delete collection: {}: {}".format(
-                    col.absolute_url(), str(e))
+        if col.getPhysicalPath() not in cols_removed:
+            terminated = [df for df in col.dataflow_uris
+                        if engine.dataflow_lookup(df).get('terminated') == '1']
+            if terminated and len(terminated) == len(col.dataflow_uris):
+                print "Removing {}. Terminated obligations: {}".format(
+                        col.absolute_url(),
+                        terminated)
+                col_par = col.getParentNode()
+                changed = False
+                if not getattr(col_par, 'can_move_released', False):
+                    col_par.can_move_released = True
+                    changed = True
+                print "Parent collection: {}".format(col_par.absolute_url())
+                try:
+                    cols_removed.append(col.getPhysicalPath())
+                    col_par.manage_delObjects(col.getId())
+                    if changed:
+                        del col_par.can_move_released
+                    col_count += 1
+                except Exception as e:
+                    print "Unable to delete collection: {}: {}".format(
+                        col.absolute_url(), str(e))
+            else:
+                print "Removing {}. Last modified date: {}".format(
+                    env.absolute_url(),
+                    env.bobobase_modification_time())
+                changed = False
+                if not getattr(col, 'can_move_released', False):
+                    col.can_move_released = True
+                    changed = True
+                try:
+                    envs_removed.append(env.absolute_url())
+                    col.manage_delObjects(env.getId())
+                    if changed:
+                        del col.can_move_released
+                    env_count += 1
+                except Exception as e:
+                    print "Something went wrong: {}".format(str(e))
         else:
-            print "Removing {}. Last modified date: {}".format(
-                env.absolute_url(),
-                env.bobobase_modification_time())
-            changed = False
-            if not getattr(col, 'can_move_released', False):
-                col.can_move_released = True
-                changed = True
-            try:
-                col.manage_delObjects(env.getId())
-                if changed:
-                    del col.can_move_released
-                env_count += 1
-            except Exception as e:
-                print "Something went wrong: {}".format(str(e))
+            print "Parent collection for: {} already deleted".format(
+                env.absolute_url())
+            env_count += 1
+
     transaction.commit()
     print "Removed {} collections and {} envelopes".format(col_count,
                                                            env_count)
+    print "Processed: \n{}\nRemoved: \n{}".format(processed, envs_removed)
 
 
 def main():
