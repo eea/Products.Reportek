315ce6e9b04d7bb7ccee2b7a71f5456d373c8fe7
olimpiurob <olimpiu.rob@gmail.com>
2023-07-04 17:08:27 +0300

    fix: avoid getting FactoryDispatcher and ProductDispatcher as app in plone.protect patch

---
+++ Products/Reportek/patches/patch_plone_protect_auto.py
@@ -0,0 +1,22 @@
+# # -*- coding: utf-8 -*-
+import itertools
+
+from App.FactoryDispatcher import FactoryDispatcher, ProductDispatcher
+
+
+def _patched_registered_objects(self):
+    # import pdb;pdb.set_trace()
+    app = self.request.PARENTS[-1]
+    # Ugly check to avoid issues when the parent is a *Dispatcher type
+    if isinstance(app, FactoryDispatcher):
+        app = self.request.PARENTS[-2]
+        if isinstance(app, ProductDispatcher):
+            app = self.request.PARENTS[-3]
+
+    return list(itertools.chain.from_iterable([
+        conn._registered_objects
+        # skip the 'temporary' connection since it stores session objects
+        # which get written all the time
+        for name, conn in app._p_jar.connections.items()
+        if name != 'temporary'
+    ]))
