8ea856bae6c9622da183c7b24ec6eb7a5ee0f637
Olimpiu Rob <olimpiu.rob@gmail.com>
2024-04-30 11:01:30 +0300

    feat: added new route for ecr user companies retrieval - refs #266920

--- Products/Reportek/ReportekUserFactoryPlugin.py
+++ Products/Reportek/ReportekUserFactoryPlugin.py
@@ -1,29 +1,30 @@
 from AccessControl import ClassSecurityInfo
 from App.class_init import InitializeClass
+from zope.interface import implements
+
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Products.PluggableAuthService.interfaces.plugins import IUserFactoryPlugin
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 from Products.Reportek.ReportekPropertiedUser import ReportekPropertiedUser
-from zope.interface import implements
 
 __all__ = [
-    'ReportekUserFactoryPlugin',
-    'manage_addReportekUserFactoryPluginForm',
-    'addReportekUserFactoryPlugin',
+    "ReportekUserFactoryPlugin",
+    "manage_addReportekUserFactoryPluginForm",
+    "addReportekUserFactoryPlugin",
 ]
 
 
 class ReportekUserFactoryPlugin(BasePlugin):
     implements(IUserFactoryPlugin)
 
-    meta_type = 'Reportek User Factory Plugin'
+    meta_type = "Reportek User Factory Plugin"
     security = ClassSecurityInfo()
 
     def __init__(self, id, title=None):
         self._setId(id)
         self.title = title
 
-    security.declarePrivate('createUser')
+    security.declarePrivate("createUser")
 
     def createUser(self, user_id, name):
         # here we can check if this user has information in the middleware
@@ -33,21 +34,23 @@ class ReportekUserFactoryPlugin(BasePlugin):
 
 
 manage_addReportekUserFactoryPluginForm = PageTemplateFile(
-    'zpt/reportekufAdd', globals(),
-    __name__='manage_addReportekUserFactoryPluginForm')
+    "zpt/reportekufAdd",
+    globals(),
+    __name__="manage_addReportekUserFactoryPluginForm",
+)
 
 
-def addReportekUserFactoryPlugin(dispatcher, id, title='', RESPONSE=None):
-    """ Add a Local Role Plugin to 'dispatcher'.
-    """
+def addReportekUserFactoryPlugin(dispatcher, id, title="", RESPONSE=None):
+    """Add a Local Role Plugin to 'dispatcher'."""
 
     plugin = ReportekUserFactoryPlugin(id, title)
     dispatcher._setObject(id, plugin)
 
     if RESPONSE is not None:
-        RESPONSE.redirect('%s/manage_main?manage_tabs_message=%s' %
-                          (dispatcher.absolute_url(),
-                           'ReportekUserFactory+added.'))
+        RESPONSE.redirect(
+            "%s/manage_main?manage_tabs_message=%s"
+            % (dispatcher.absolute_url(), "ReportekUserFactory+added.")
+        )
 
 
 InitializeClass(ReportekUserFactoryPlugin)
