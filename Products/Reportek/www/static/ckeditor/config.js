c96362c7b47d6c2c87b00bad1017255ae740f06c
Olimpiu Rob <olimpiu.rob@gmail.com>
2024-11-27 18:57:01 +0200

    Disabled jslint on ckeditor

--- Products/Reportek/www/static/ckeditor/config.js
+++ Products/Reportek/www/static/ckeditor/config.js
@@ -1,3 +1,4 @@
+/* jslint:disable */
 /**
  * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
--- Products/Reportek/www/static/ckeditor/plugins/a11yhelp/dialogs/lang/pl.js
+++ Products/Reportek/www/static/ckeditor/plugins/a11yhelp/dialogs/lang/pl.js
@@ -1,3 +1,4 @@
+/* jslint:disable */
 ﻿/*
  Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
  For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
