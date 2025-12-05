1b670595bd3a89368749c48942b837f94e39d793
Olimpiu Rob <olimpiu.rob@gmail.com>
2025-12-05 12:42:43 +0200

    fix: fixed zip

--- Products/Reportek/gzipraw.py
+++ Products/Reportek/gzipraw.py
@@ -3,27 +3,37 @@ import gzip
 
 class GzipFileRaw(gzip.GzipFile):
     # inherit GzipFile init. give both filenam and fileobj opened for writing
-    def __init__(self, filename=None, mode=None,
-                 compresslevel=9, fileobj=None, mtime=None, crc=None,
-                 orig_size=None):
-        if mode != 'w' and mode != 'wb':
+    def __init__(
+        self,
+        filename=None,
+        mode=None,
+        compresslevel=9,
+        fileobj=None,
+        mtime=None,
+        crc=None,
+        orig_size=None,
+    ):
+        if mode != "w" and mode != "wb":
             raise NotImplementedError("GzipFileRaw is only for writing")
         # if not (filename and fileobj):
         #    raise ValueError("Need to know the name of the file inside zip.
         #    provide both filename and fileobj")
         if not (crc and orig_size):
             ValueError(
-                '''Supply CRC and original size from the already compressed'''
-                ''' archive the content stems from''')
+                """Supply CRC and original size from the already compressed"""
+                """ archive the content stems from"""
+            )
         super(GzipFileRaw, self).__init__(
-            filename, mode, compresslevel, fileobj, mtime)
+            filename, mode, compresslevel, fileobj, mtime
+        )
         self.crc = crc
         self.size = orig_size
 
     def write(self, compressedBuffer):
-        self._check_closed()
+        self._checkClosed()
         if self.mode != gzip.WRITE:
             import errno
+
             raise IOError(errno.EBADF, "write() on read-only GzipFile object")
 
         if len(compressedBuffer) > 0:
@@ -42,7 +52,7 @@ class GzipFileRaw(gzip.GzipFile):
         # Make sure unittets always pass.
         gzip.write32u(self.fileobj, self.crc)
         # gzip.write32u(self.fileobj, self.size & 0xffffffffL)
-        gzip.write32u(self.fileobj, self.size & 0xffffffff)
+        gzip.write32u(self.fileobj, self.size & 0xFFFFFFFF)
         self.fileobj = None
         if self.myfileobj:
             self.myfileobj.close()
