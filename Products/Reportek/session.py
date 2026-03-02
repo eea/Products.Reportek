611373b25a50636cef46cd21f1f25ebcc817c6a8
Olimpiu Rob <olimpiu.rob@gmail.com>
2026-03-02 18:18:11 +0200

    fix: fixed session.type, added redis proto, renamed session.key to work with existing varnish.vcl

--- Products/Reportek/session.py
+++ Products/Reportek/session.py
@@ -20,17 +20,17 @@ def beaker_session_filter_factory(app, global_conf, **local_conf):
         logger.info("Beaker session backend is DISABLED. Passing WSGI app through unmodified.")
         return app
 
-    redis_url = os.environ.get('REDIS_SESSION_URL', 'redis:6379')
+    redis_url = os.environ.get('REDIS_SESSION_URL', 'redis://redis:6379/0')
     secret = os.environ.get('SESSION_SECRET', 'secret_key')
 
     logger.info("Injecting Beaker session backend via Redis: {0}".format(redis_url))
     
     # Configure Beaker to use Redis
     session_opts = {
-        'session.type': 'redis',
+        'session.type': 'ext:redis',
         'session.url': redis_url,
         'session.secret': secret,
-        'session.key': 'beaker.session.id',
+        'session.key': 'beaker.session',
         'session.auto': True,
         'session.cookie_expires': True, # expire on browser close by default
         # Add timeout or max_age if persistent sessions are needed
