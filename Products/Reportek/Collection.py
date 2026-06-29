805132e9ecea277ff7c053d80e90d62fde0ea0d2
eea-jenkins <eea-github@googlegroups.com>
2026-06-29 15:10:48 +0200

    style: Automated code fix

--- Products/Reportek/Collection.py
+++ Products/Reportek/Collection.py
@@ -211,9 +211,7 @@ class BaseCollection(ReportekContent):
     """BaseCollection class."""
 
 
-class Collection(
-    CatalogAware, Folder, Toolz, DFlowCatalogAware, BaseCollection
-):
+class Collection(CatalogAware, Folder, Toolz, DFlowCatalogAware, BaseCollection):
     """
     Collections are basic container objects that provide a standard
     interface for object management. Collection objects also implement
@@ -332,9 +330,7 @@ class Collection(
 
     def manage_main(self, *args, **kw):
         """Define manage main to be context aware"""
-        if getSecurityManager().checkPermission(
-            "View management screens", self
-        ):
+        if getSecurityManager().checkPermission("View management screens", self):
             return self.manage_main_inh(*args, **kw)
         else:
             return self.index_html(*args, **kw)
@@ -372,15 +368,11 @@ class Collection(
     )
 
     security.declareProtected("View", "company_details")
-    company_details = PageTemplateFile(
-        "zpt/collection/company_details.zpt", globals()
-    )
+    company_details = PageTemplateFile("zpt/collection/company_details.zpt", globals())
 
     security.declareProtected("View", "other_reports")
     other_reports = PageTemplateFile(
-        os.path.join(
-            package_home(globals()), "zpt/collection/other_reports.zpt"
-        )
+        os.path.join(package_home(globals()), "zpt/collection/other_reports.zpt")
     )
 
     def local_defined_roles(self):
@@ -392,9 +384,7 @@ class Collection(
 
     def local_unique_roles(self):
         return set(
-            role
-            for roles in list(self.__ac_local_roles__.values())
-            for role in roles
+            role for roles in list(self.__ac_local_roles__.values()) for role in roles
         )
 
     security.declareProtected(manage_users, "get_users_list")
@@ -423,9 +413,7 @@ class Collection(
                         global_members[member]["roles"].append(role)
                     else:
                         global_members[member] = {
-                            "type": "group"
-                            if member in ldap_groups
-                            else "user",
+                            "type": "group" if member in ldap_groups else "user",
                             "roles": [role],
                         }
         # retrieve the local accounts
@@ -439,9 +427,7 @@ class Collection(
                     if role_param and role_param in ["Reporter", "Client"]:
                         if role == role_param:
                             if member in members:
-                                members[member]["roles"].append(
-                                    [context, [role]]
-                                )
+                                members[member]["roles"].append([context, [role]])
                             else:
                                 u_type = "user"
                                 if member in ldap_groups:
@@ -453,9 +439,7 @@ class Collection(
                     else:
                         if role in ["Reporter", "Client"]:
                             if member in members:
-                                members[member]["roles"].append(
-                                    [context, list(roles)]
-                                )
+                                members[member]["roles"].append([context, list(roles)])
                             else:
                                 u_type = "user"
                                 if member in ldap_groups:
@@ -560,9 +544,7 @@ class Collection(
             return dataflow_uris
 
         except Exception as e:
-            logger.exception(
-                "Error retrieving dataflow URIs: {}".format(str(e))
-            )
+            logger.exception("Error retrieving dataflow URIs: {}".format(str(e)))
             return []
 
     security.declarePublic("num_terminated_dataflows")
@@ -730,9 +712,7 @@ class Collection(
             l_ret = p_parameter + "=" + str(p_value)
         return l_ret
 
-    def changeQueryString2(
-        self, p_query_string, p_parameter=None, p_value=None
-    ):
+    def changeQueryString2(self, p_query_string, p_parameter=None, p_value=None):
         """given the QUERY_STRING part of an URL, the function does the
         following:
         - if type(p_parameter) is str  searches for the parameter
@@ -746,13 +726,10 @@ class Collection(
             p_query_string, p_parameter, p_value
         )
         return "&".join(
-            str(x) + "=" + str(l_query_array[x])
-            for x in list(l_query_array.keys())
+            str(x) + "=" + str(l_query_array[x]) for x in list(l_query_array.keys())
         )
 
-    def changeQueryString2Dict(
-        self, p_query_string, p_parameter=None, p_value=None
-    ):
+    def changeQueryString2Dict(self, p_query_string, p_parameter=None, p_value=None):
         """returns the array for changeQueryString2"""
         # store the {key,value} pair in a dictionary
         l_query_array = {}
@@ -813,9 +790,7 @@ class Collection(
                 parent = obj.getParentNode()
             except Exception:
                 return (None, None)
-            if hasattr(obj, "_company_id") and not hasattr(
-                parent, "_company_id"
-            ):
+            if hasattr(obj, "_company_id") and not hasattr(parent, "_company_id"):
                 return (obj.title, obj.getId())
             else:
                 return cname(parent)
@@ -826,9 +801,7 @@ class Collection(
 
     def messageDialog(self, message="", action="", params=None, REQUEST=None):
         """displays a message dialog"""
-        return self.message_dialog(
-            message=message, action=action, params=params
-        )
+        return self.message_dialog(message=message, action=action, params=params)
 
     message_dialog = PageTemplateFile(
         os.path.join(package_home(globals()), "zpt/message_dialog.zpt")
@@ -895,9 +868,7 @@ class Collection(
                 request = None
 
             if request is not None:
-                cache_key = "_get_company_data_{}".format(
-                    self.absolute_url_path()
-                )
+                cache_key = "_get_company_data_{}".format(self.absolute_url_path())
                 cached = request.get(cache_key)
                 if cached is not None:
                     return cached
@@ -912,9 +883,7 @@ class Collection(
                         if obl in engine.er_ods_obligations:
                             domain = "ODS"
                             break
-                    data = registry.get_company_details(
-                        self.company_id, domain=domain
-                    )
+                    data = registry.get_company_details(self.company_id, domain=domain)
                 else:
                     # For BDR-Registry, get the domain from the top-level path
                     domain = (
@@ -922,9 +891,7 @@ class Collection(
                         if len(self.getPhysicalPath()) > 1
                         else None
                     )
-                    data = registry.get_company_details(
-                        self.company_id, domain=domain
-                    )
+                    data = registry.get_company_details(self.company_id, domain=domain)
                 if data:
                     data["registry"] = registry_name
 
@@ -1081,9 +1048,7 @@ class Collection(
                     if not isinstance(data, dict):
                         res["result"] = "Fail"
                         res["message"] = "Malformed body"
-                    response = registry.get_company_paus(
-                        self.company_id, domain=domain
-                    )
+                    response = registry.get_company_paus(self.company_id, domain=domain)
                     if response is not None:
                         if response.status_code != requests.codes.ok:
                             res["result"] = "Fail"
@@ -1334,9 +1299,7 @@ class Collection(
                 alt_street = alt_street + " " + alt_street_no
 
             street = raw_data.get("addr_street", alt_street)
-            city = raw_data.get(
-                "addr_place1", raw_data.get("address", {}).get("city")
-            )
+            city = raw_data.get("addr_place1", raw_data.get("address", {}).get("city"))
             country = raw_data.get("country", raw_data.get("country_code"))
             data = {
                 "name": raw_data.get("name"),
@@ -1484,9 +1447,7 @@ class Collection(
             request = None
 
         if request is not None:
-            cache_key = "_get_domain_{}_{}".format(
-                self.absolute_url_path(), df_type
-            )
+            cache_key = "_get_domain_{}_{}".format(self.absolute_url_path(), df_type)
             cached = request.get(cache_key)
             if cached is not None:
                 return cached
@@ -1543,9 +1504,7 @@ class Collection(
         from zope.interface import alsoProvides
 
         if hasattr(plone.protect.interfaces, "IDisableCSRFProtection"):
-            alsoProvides(
-                self.REQUEST, plone.protect.interfaces.IDisableCSRFProtection
-            )
+            alsoProvides(self.REQUEST, plone.protect.interfaces.IDisableCSRFProtection)
         # This is a bit weird here, since we expect this to be called
         # from envelopes from audit collection, which is a
         # subcollection of the FGAS collection.
@@ -1586,9 +1545,7 @@ class Collection(
                     if key in metadata:
                         amount = metadata.get(key, {}).get("Amount")
                         try:
-                            mdata[key] = (
-                                Decimal(amount) if amount is not None else None
-                            )
+                            mdata[key] = Decimal(amount) if amount is not None else None
                         except (
                             InvalidOperation,
                             ValueError,
